10dc5c813cb9d1e0321b308ba6d92960d662ec73
Ash469 <ashshandilya4@gmail.com>
2025-02-15 19:56:56 +0530

    connect backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json()); 
 app.use(
   cors({
-    origin: "https://quiz-deploy-1.onrender.com/", // Allow only your frontend
+    origin: "https://quiz-deploy-1.onrender.com", // Allow only your frontend
     credentials: true,
   })
 );
